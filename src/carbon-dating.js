ea3a65539816a9508603acc86c586ae4a31c3f2b
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-10-29 22:33:37 +0300

    feat: add code of cats and carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,20 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false
+  }
+  if (sampleActivity.trim() === '') {
+    return false
+  }
+
+  const sampleActivityNum = Number(sampleActivity);
+  if (isNaN(sampleActivityNum)) {
+    return false
+  }
+  if (sampleActivityNum > MODERN_ACTIVITY || sampleActivityNum <= 0) {
+    return false
+  }
+  return MODERN_ACTIVITY / sampleActivity
 };
