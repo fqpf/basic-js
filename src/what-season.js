ba251bc20386ec2d07336a24236dd2dca0497026
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-10-30 19:58:59 +0300

    feat: add code of what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d) && d.toString() === '[object Date]';
+}
+
+module.exports = function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+    if (!isValidDate(date)) {
+      return 'THROWN';
+    }
+    const numOfMonth = date.getMonth();
+    if ([0, 1, 11].includes(numOfMonth)) return 'winter';
+    if ([2, 3, 4].includes(numOfMonth)) return 'spring';
+    if ([5, 6, 7].includes(numOfMonth)) return 'summer';
+    return 'fall';
 };
