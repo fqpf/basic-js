5251f75a07d48ef43a6a179277167d06f415e7c9
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-10-30 19:36:21 +0300

    feat: add code of dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let newArr = [];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let el of members) {
+    if (typeof el === 'string' ) {
+      let firstWord = el.trim().slice(0, 1).toUpperCase();
+      newArr.push(firstWord);
+    }
+  }
+  return newArr.sort().join('')
 };
