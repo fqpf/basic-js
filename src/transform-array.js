a0f49e26dc4e9c22c121ba3463b18f08ea685459
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-11-01 16:09:23 +0300

    feat: add code of transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const res = [...arr];
+  for (let i = 0; i < res.length; i++) {
+    switch (res[i]) {
+      case '--double-next':
+        if (i !== res.length - 1) {
+          res[i] = res[i + 1];
+        } else {
+          res[i] = '--remove';
+        }
+        break;
+      case '--double-prev':
+        if (i !== 0) {
+          res[i] = res[i - 1];
+        } else {
+          res[i] = '--remove';
+        }
+        break;
+      case '--discard-prev':
+        if (i !== 0) {
+          res[i - 1] = '--remove';
+        }
+        res[i] = '--remove';
+        break;
+      case '--discard-next':
+        if (i !== res.length - 1) {
+          res[i + 1] = '--remove';
+        }
+        res[i] = '--remove';
+        break;
+    }
+  }
+  return res.filter(el => el !== '--remove')
 };
